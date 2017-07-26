9915878fdd6330189f909ffa08ff8f32bc8202a9
krapimimhannah <hannahmimipark@gmail.com>
2017-07-26 15:35:28 -0400

    update to take playlist of lyrics

--- read.py
+++ read.py
@@ -1,16 +1,28 @@
 import re
 import sys
+import os
 
 from stop_words import get_stop_words
 from nltk.tokenize import RegexpTokenizer
 from gensim import corpora, models
 
 
-def run_lda(filepath, num_topics=2, num_words=10):
+def playlist_lda(directory, num_topics=2, num_words=10):
+    tokenized_text = []
+
+    for filename in os.listdir(directory):
+        filepath = os.path.join(directory, filename)
+        tokens = preprocess_text(filepath)
+        tokenized_text.append(tokens)
+
+    get_lda_topics(tokenized_text, num_topics, num_words)
+
+
+def preprocess_text(filepath):
     lyrics = read_lyrics(filepath)
     tokens = tokenize_by_word(lyrics)
     tokenized_text = clean_stop_words(tokens)
-    get_lda_topics([tokenized_text], num_topics, num_words)
+    return tokenized_text
 
 
 def get_lda_topics(array_of_lyrics, num_topics, num_words):
@@ -28,7 +40,7 @@ def tokenize_by_word(lyrics):
     tokenizer = RegexpTokenizer(r'\w+')
     tokens = []
     for line in lyrics:
-        raw = line.lower()
+        raw = line.lower().decode('utf-8')
         raw_tokens = tokenizer.tokenize(raw)
         tokens = tokens + raw_tokens
     return tokens
@@ -56,13 +68,3 @@ def read_lyrics(filename):
     strip_verse_markers = [pattern.sub('', line.strip()) for line in lyrics]
     content = [line for line in strip_verse_markers if line != '']
     return content
-
-#
-# if __name__ == '__main__':
-#     if len(sys.argv) > 1:
-#         filename = sys.argv[1]
-#     else:
-#         print("Usage: %s filename" % (sys.argv[0],))
-#         sys.exit()
-#
-#     read_lyrics(filename)
