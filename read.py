ca3d92f201dd59fa412edaf65b054ac5d535059f
krapimimhannah <hannahmimipark@gmail.com>
2017-07-26 15:17:47 -0400

    functions for running LDA

---
+++ read.py
@@ -0,0 +1,68 @@
+import re
+import sys
+
+from stop_words import get_stop_words
+from nltk.tokenize import RegexpTokenizer
+from gensim import corpora, models
+
+
+def run_lda(filepath, num_topics=2, num_words=10):
+    lyrics = read_lyrics(filepath)
+    tokens = tokenize_by_word(lyrics)
+    tokenized_text = clean_stop_words(tokens)
+    get_lda_topics([tokenized_text], num_topics, num_words)
+
+
+def get_lda_topics(array_of_lyrics, num_topics, num_words):
+    dictionary = corpora.Dictionary(array_of_lyrics)
+    corpus = [dictionary.doc2bow(text) for text in array_of_lyrics]
+    ldamodel = models.ldamodel.LdaModel(
+        corpus,
+        num_topics,
+        id2word=dictionary,
+        passes=20)
+    print(ldamodel.print_topics(num_topics, num_words))
+
+
+def tokenize_by_word(lyrics):
+    tokenizer = RegexpTokenizer(r'\w+')
+    tokens = []
+    for line in lyrics:
+        raw = line.lower()
+        raw_tokens = tokenizer.tokenize(raw)
+        tokens = tokens + raw_tokens
+    return tokens
+
+
+def clean_stop_words(tokenized_lyrics):
+    en_stop = get_stop_words('en')
+    en_stop.append('yeah')
+    stopped_tokens = [
+        token for token
+        in tokenized_lyrics
+        if token not in en_stop and len(token) > 1]
+    # remove single letters, does not take
+    # care of contractions (e.g. I'm very well)
+    return stopped_tokens
+
+
+def read_lyrics(filename):
+    with open(filename) as f:
+        lyrics = f.readlines()
+        f.close()
+
+    pattern = re.compile('\[(.*?)\]')
+
+    strip_verse_markers = [pattern.sub('', line.strip()) for line in lyrics]
+    content = [line for line in strip_verse_markers if line != '']
+    return content
+
+#
+# if __name__ == '__main__':
+#     if len(sys.argv) > 1:
+#         filename = sys.argv[1]
+#     else:
+#         print("Usage: %s filename" % (sys.argv[0],))
+#         sys.exit()
+#
+#     read_lyrics(filename)
